e34f560c168b2e81c384e695b18eb9b4c5ffffda
goldze <goldze@126.com>
2018-04-19 14:56:00 +0800

    add command
    添加EditText绑定命令

--- mvvmhabit/src/main/java/me/goldze/mvvmhabit/binding/viewadapter/edittext/ViewAdapter.java
+++ mvvmhabit/src/main/java/me/goldze/mvvmhabit/binding/viewadapter/edittext/ViewAdapter.java
@@ -2,8 +2,12 @@ package me.goldze.mvvmhabit.binding.viewadapter.edittext;
 
 import android.content.Context;
 import android.databinding.BindingAdapter;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+
+import me.goldze.mvvmhabit.binding.command.BindingCommand;
 
 /**
  * Created by goldze on 2017/6/16.
@@ -13,7 +17,7 @@ public class ViewAdapter {
     /**
      * EditText重新获取焦点的事件绑定
      */
-    @BindingAdapter({"requestFocus"})
+    @BindingAdapter(value = {"requestFocus"}, requireAll = false)
     public static void requestFocusCommand(EditText editText, final Boolean needRequestFocus) {
         if (needRequestFocus) {
             editText.setSelection(editText.getText().length());
@@ -21,6 +25,31 @@ public class ViewAdapter {
             InputMethodManager imm = (InputMethodManager) editText.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT);
         }
-		editText.setFocusableInTouchMode(needRequestFocus);
+        editText.setFocusableInTouchMode(needRequestFocus);
+    }
+
+    /**
+     * EditText输入文字改变的监听
+     */
+    @BindingAdapter(value = {"textChanged"}, requireAll = false)
+    public static void addTextChangedListener(EditText editText, final BindingCommand<String> textChanged) {
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence text, int i, int i1, int i2) {
+                if (textChanged != null) {
+                    textChanged.execute(text.toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
