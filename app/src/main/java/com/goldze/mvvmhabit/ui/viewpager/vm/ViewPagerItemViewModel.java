694a32c7e4af9ec98091e7ec522f99ef3c38b9cc
goldze <goldze@126.com>
2019-02-17 13:29:04 +0800

    supplement
    supplement

--- app/src/main/java/com/goldze/mvvmhabit/ui/viewpager/vm/ViewPagerItemViewModel.java
+++ app/src/main/java/com/goldze/mvvmhabit/ui/viewpager/vm/ViewPagerItemViewModel.java
@@ -9,6 +9,7 @@ import me.goldze.mvvmhabit.binding.command.BindingCommand;
 import me.goldze.mvvmhabit.bus.event.SingleLiveEvent;
 
 /**
+ * 所有例子仅做参考,千万不要把它当成一种标准,毕竟主打的不是例子,业务场景繁多,理解如何使用才最重要。
  * Created by goldze on 2018/7/18.
  */
 
