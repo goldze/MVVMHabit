96ed5322ed10b54dcf93bb5a15494fd9c1c6458f
zengxianze <603883033@qq.com>
2019-10-21 14:26:15 +0800

    Solve demo memory leak
    Solve demo memory leak

--- app/src/main/java/com/goldze/mvvmhabit/ui/tab_bar/activity/TabBarActivity.java
+++ app/src/main/java/com/goldze/mvvmhabit/ui/tab_bar/activity/TabBarActivity.java
@@ -55,9 +55,7 @@ public class TabBarActivity extends BaseActivity<ActivityTabBarBinding, BaseView
         mFragments.add(new TabBar3Fragment());
         mFragments.add(new TabBar4Fragment());
         //默认选中第一个
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.add(R.id.frameLayout, mFragments.get(0));
-        transaction.commitAllowingStateLoss();
+        commitAllowingStateLoss(0);
     }
 
     private void initBottomTab() {
@@ -72,9 +70,10 @@ public class TabBarActivity extends BaseActivity<ActivityTabBarBinding, BaseView
         navigationController.addTabItemSelectedListener(new OnTabItemSelectedListener() {
             @Override
             public void onSelected(int index, int old) {
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.frameLayout, mFragments.get(index));
-                transaction.commitAllowingStateLoss();
+//                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//                transaction.replace(R.id.frameLayout, mFragments.get(index));
+//                transaction.commitAllowingStateLoss();
+                commitAllowingStateLoss(index);
             }
 
             @Override
@@ -82,13 +81,28 @@ public class TabBarActivity extends BaseActivity<ActivityTabBarBinding, BaseView
             }
         });
     }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (mFragments != null) {
-            mFragments.clear();
-            mFragments = null;
+    private void commitAllowingStateLoss(int position) {
+        hideAllFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        Fragment currentFragment = getSupportFragmentManager().findFragmentByTag(position + "");
+        if (currentFragment != null) {
+            transaction.show(currentFragment);
+        } else {
+            currentFragment = mFragments.get(position);
+            transaction.add(R.id.frameLayout, currentFragment, position + "");
         }
+        transaction.commitAllowingStateLoss();
+    }
+
+    //隐藏所有Fragment
+    private void hideAllFragment() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        for (int i = 0; i < mFragments.size(); i++) {
+            Fragment currentFragment = getSupportFragmentManager().findFragmentByTag(i + "");
+            if (currentFragment != null) {
+                transaction.hide(currentFragment);
+            }
+        }
+        transaction.commitAllowingStateLoss();
     }
 }
