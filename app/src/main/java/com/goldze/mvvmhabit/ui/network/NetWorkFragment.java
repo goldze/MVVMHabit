da06fdc730edeef98fd6c5a6e05d6eac479e47c3
goldze <603883033@qq.com>
2021-07-16 10:13:18 +0800

    升级bindingcollectionadapter，优化demo绑定方式

--- app/src/main/java/com/goldze/mvvmhabit/ui/network/NetWorkFragment.java
+++ app/src/main/java/com/goldze/mvvmhabit/ui/network/NetWorkFragment.java
@@ -52,9 +52,6 @@ public class NetWorkFragment extends BaseFragment<FragmentNetworkBinding, NetWor
 
     @Override
     public void initData() {
-        //给RecyclerView添加Adpter，请使用自定义的Adapter继承BindingRecyclerViewAdapter，重写onBindBinding方法，里面有你要的Item对应的binding对象。
-        // Adapter属于View层的东西, 不建议定义到ViewModel中绑定，以免内存泄漏
-        binding.setAdapter(new BindingRecyclerViewAdapter());
         //请求网络数据
         viewModel.requestNetWork();
     }
--- app/src/main/java/com/goldze/mvvmhabit/ui/rv_multi/MultiRecycleViewFragment.java
+++ app/src/main/java/com/goldze/mvvmhabit/ui/rv_multi/MultiRecycleViewFragment.java
@@ -32,8 +32,5 @@ public class MultiRecycleViewFragment extends BaseFragment<FragmentMultiRvBindin
     @Override
     public void initData() {
         super.initData();
-        //给RecyclerView添加Adpter，请使用自定义的Adapter继承BindingRecyclerViewAdapter，重写onBindBinding方法，里面有你要的Item对应的binding对象。
-        // Adapter属于View层的东西, 不建议定义到ViewModel中绑定，以免内存泄漏
-        binding.setAdapter(new BindingRecyclerViewAdapter());
     }
 }
